44dc042e09d0514e72374eccaf25ad26dc0108aa
Evan Jeffries <ejeffries14@gmail.com>
2019-11-02 12:12:39 +0000

    first commit

---
+++ lib/drug_shortage.rb
@@ -0,0 +1,6 @@
+require "drug_shortage/version"
+
+module DrugShortage
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/drug_shortage/version.rb
@@ -0,0 +1,3 @@
+module DrugShortage
+  VERSION = "0.1.0"
+end

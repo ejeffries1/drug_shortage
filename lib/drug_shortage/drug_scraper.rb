07bc7f811ffa2baea475a6b50dbc8195c043883d
Evan Jeffries <ejeffries14@gmail.com>
2019-11-03 15:24:21 +0000

    New Code

--- lib/drug_shortage/drug_scraper.rb
+++ lib/drug_shortage/drug_scraper.rb
@@ -1,4 +1,11 @@
 class Drug
 
-  
+  def test
+    puts "I'm working"
+  end
+
+  def scraper
+    @doc = NOKOGIRI::HTML(open("https://www.ashp.org/Drug-Shortages/Current-Shortages/Drug-Shortages-List?page=CurrentShortages&sort=2"))
+    binding.pry
+  end
 end

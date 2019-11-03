80a44c7b58c92279c369bad34eec957d0325ecc3
Evan Jeffries <ejeffries14@gmail.com>
2019-11-03 09:11:05 +0000

    bundle

--- lib/drug_shortage.rb
+++ lib/drug_shortage.rb
@@ -1,2 +1,7 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
 require "./lib/drug_shortage/version"
+require_relative './drug_shortage/drug_scraper'
 require_relative './drug_shortage/cli'

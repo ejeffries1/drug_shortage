9f9f237446b4408b033ea08500e7aaeda0a0339d
Evan Jeffries <ejeffries14@gmail.com>
2019-11-04 16:00:57 +0000

    Drug_scraper  update

--- lib/drug_shortage/drug_scraper.rb
+++ lib/drug_shortage/drug_scraper.rb
@@ -1,4 +1,5 @@
 class Drug
+  attr_accessor :name, :availability
 
   def self.test
     puts "I'm working"
@@ -6,6 +7,10 @@ class Drug
 
   def self.current
     @doc = Nokogiri::HTML(open("https://www.ashp.org/Drug-Shortages/Current-Shortages/Drug-Shortages-List?page=CurrentShortages&sort=2"))
+      @doc.css(".odd").each do |tag|
+         name = tag.css("a").text
+         availability = tag.css(".right").text
     binding.pry
+    end
   end
 end

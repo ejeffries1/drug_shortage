d18332a50bbc596728463534fc41bf9c42940651
Evan Jeffries <ejeffries14@gmail.com>
2019-11-04 16:15:16 +0000

    modified

--- lib/drug_shortage/drug_scraper.rb
+++ lib/drug_shortage/drug_scraper.rb
@@ -5,12 +5,16 @@ class Drug
     puts "I'm working"
   end
 
+  @@newdrug = []
+  @@date = []
   def self.current
     @doc = Nokogiri::HTML(open("https://www.ashp.org/Drug-Shortages/Current-Shortages/Drug-Shortages-List?page=CurrentShortages&sort=2"))
       @doc.css(".odd").each do |tag|
-         name = tag.css("a").text
-         availability = tag.css(".right").text
+         @@newdrug << name = tag.css("a").text
+         @@date << availability = tag.css(".right").text
     binding.pry
     end
   end
+  @@current
+  @@date
 end

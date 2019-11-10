f5e11dbb09ae5dabe741a153910f4ad01e3c2eb5
Evan Jeffries <ejeffries14@gmail.com>
2019-11-10 20:11:08 +0000

    update

--- lib/drug_shortage/drug_scraper.rb
+++ lib/drug_shortage/drug_scraper.rb
@@ -4,11 +4,13 @@ class DrugShortage::Drug
     @all = []
   def self.scrape_current
     @doc = Nokogiri::HTML(open("https://www.ashp.org/Drug-Shortages/Current-Shortages/Drug-Shortages-List?page=CurrentShortages&sort=2"))
+      @doc.css(".drug-shortage-container").collect do |list|
       drug = self.new
-      drug.name = @doc.css("tr.odd").text.gsub(/\s+/mu," ").strip
+      drug.name = list.css("tr.odd").text.gsub(/\s+/mu,",").strip
       @all << drug.name
+    end
       @all
-      binding.pry
+      #binding.pry
   end
 
 

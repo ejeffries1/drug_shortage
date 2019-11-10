092ddccd8b22919b561c8260c21ef97df558bc9b
Evan Jeffries <ejeffries14@gmail.com>
2019-11-10 13:36:20 +0000

    Scraper update

--- lib/drug_shortage/drug_scraper.rb
+++ lib/drug_shortage/drug_scraper.rb
@@ -2,20 +2,24 @@ class DrugShortage::Drug
   attr_accessor :name, :availability
 
   def self.tester
-    puts "This method is working. Goodluck!"
-    drug_1 = self.new
-    drug_1.name = "Atorvastatin-Aug 2019"
+    @all
+    #puts "This method is working. Goodluck!"
+    #drug_1 = self.new
+    #drug_1.name = "Atorvastatin-Aug 2019"
 
-    drug_2 = self.new
-    drug_2.name = "Lisinopril-Nov 2019"
+    #drug_2 = self.new
+    #drug_2.name = "Lisinopril-Nov 2019"
 
-    [drug_1, drug_2]
+    #[drug_1, drug_2]
   end
 
   def self.scrape_current
     @doc = Nokogiri::HTML(open("https://www.ashp.org/Drug-Shortages/Current-Shortages/Drug-Shortages-List?page=CurrentShortages&sort=2"))
-      name = @doc.css("td[align='left']").text
-      availability = @doc.css("td.right").text
-    binding.pry
+      @doc.each do |list|
+      drug = self.new
+      drug.name = list.css("tr.odd").text.gsub(/\s+/mu," ").strip
+      @all << drug.name
+    end
+    @all
   end
 end

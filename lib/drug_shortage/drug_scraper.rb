befe6679b1fbc559182b29cc27dc7cdfbefafd80
Evan Jeffries <ejeffries14@gmail.com>
2019-11-15 08:16:20 +0000

    finished project

--- lib/drug_shortage/drug_scraper.rb
+++ lib/drug_shortage/drug_scraper.rb
@@ -1,14 +1,14 @@
 class DrugShortage::Drug
-  attr_accessor :name, :availability
+  attr_accessor :name
 
     @all = []
   def self.scrape_current
     doc = Nokogiri::HTML(open("https://www.ashp.org/Drug-Shortages/Current-Shortages/Drug-Shortages-List?page=CurrentShortages&sort=2"))
+      current = DrugShortage::Drug.new
       doc.css(".drug-shortage-container").collect do |list|
-      drug.name = list.css("tr.odd").text.gsub(/\s+/mu," ").strip
-      @all << drug.name
+      current.name = list.css("tr.odd").text.gsub(/\s+/mu," ").strip
     end
-      @all
+      current.name
   end
 
   def self.current_drug
@@ -18,20 +18,15 @@ class DrugShortage::Drug
   def self.scrape_resolve
     @resolved = []
     doc = Nokogiri::HTML(open("https://www.ashp.org/Drug-Shortages/Current-Shortages/Drug-Shortages-List?page=ResolvedShortages"))
-      doc.css(".drug-shortage-container").each do |drug|
-        name = drug.css("tr.odd").text.gsub(/\s+/mu," ").strip
+      doc.css(".drug-shortage-container").collect do |drug|
         resolve = DrugShortage::Drug.new
-         @resolved << resolve.name
-         @resolved
-        #binding.pry
+        resolve.name = drug.css("tr.odd").text.gsub(/\s+/mu," ").strip
+        resolve.name
       end
   end
 
   def self.resolved_drug
-    self.scrape_resolve.each do |i|
-      i.children.text
-      #binding.pry
+    self.scrape_resolve
     end
 
   end
-end
